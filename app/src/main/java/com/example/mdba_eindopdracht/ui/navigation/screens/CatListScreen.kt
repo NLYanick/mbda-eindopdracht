75a99f6afdceaf3754dab41765eee826fd41d6c5
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-03-27 14:53:34 +0100

    filter favourites

--- app/src/main/java/com/example/mdba_eindopdracht/ui/navigation/screens/CatListScreen.kt
+++ app/src/main/java/com/example/mdba_eindopdracht/ui/navigation/screens/CatListScreen.kt
@@ -3,6 +3,7 @@ package com.example.mdba_eindopdracht.ui.navigation.screens
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,12 +39,17 @@ fun CatListScreen(
     val repository = CatRepository(LocalContext.current)
     var cats by remember { mutableStateOf<List<CatData>>(emptyList()) }
     var isLoading by remember { mutableStateOf(true) }
+    
+    var filterFavourites by remember { mutableStateOf(false) }
+    var filteredCats by remember { mutableStateOf<List<CatData>>(emptyList()) }
 
     LaunchedEffect(Unit) { // LaunchedEffect makes it possible to run suspend/async functions
         isLoading = true
         try {
             cats = repository.fetchAllCatBreeds()
             viewModel.restoreFavoritesToList(cats)
+
+            filteredCats = cats
         } catch (e: Exception) {
             print("Error in loading cats: $e")
         } finally {
@@ -53,12 +60,25 @@ fun CatListScreen(
     if(isLoading) {
         Text(text = "Loading cats...")
     } else {
-        LazyColumn(modifier = modifier) {
-            items(items = cats) { cat ->
-                CatRow(cat, {
-                    viewModel.selectCat(cat,repository);
-                    navController.navigate(CatDetails.route)
-                })
+        Column {
+            Button(onClick = {
+                filterFavourites = !filterFavourites
+                
+                filteredCats = if(filterFavourites) {
+                    cats.filter { it.isFavourite }
+                } else {
+                    cats
+                }
+            }) {
+                Text(text = if (filterFavourites) "Remove filter" else "Filter favorites")
+            }
+            LazyColumn(modifier = modifier) {
+                items(items = filteredCats) { cat ->
+                    CatRow(cat, {
+                        viewModel.selectCat(cat,repository);
+                        navController.navigate(CatDetails.route)
+                    })
+                }
             }
         }
     }
